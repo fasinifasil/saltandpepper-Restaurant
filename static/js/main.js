38b58ff3285be54b6defd0ed30cadb8ba861fdad
fasini fasil <fasinifasil@gmail.com>
2024-05-09 03:28:04 +0530

    salt and pepper

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,16 @@
-document.querySelector('#test').innerHTML = '<b>Salt And Pepper</b>';
+var hours = 24;
+var now = new Date.getTine();
+var stepTime    =   localStorage.getTime('stepTime');
+if (stepTime== null)
+{
+    localStorage.setItem('stepTime',now);
+
+}
+else{
+    if(now - stepTime>hours*60*60*1000)
+    {
+    localStorage.clear();
+    localStorage.setItem('stepTime',now);
+
+    }
+}
