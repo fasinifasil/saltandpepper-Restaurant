7d26586fa6ce49c665f500d3c682fbc9201c7b35
fasini fasil <fasinifasil@gmail.com>
2024-05-12 22:22:56 +0530

    salt and pepper

--- BaseApp/tests.py
+++ BaseApp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from BaseApp.models import PizzaModel
+
 
 # Create your tests here.
+# class homePageTestCase(TestCase):
+#     def test_home_page(self):
+#             self.assertEqual('test1','test1')
+class homePageTestCase(TestCase):
+    def test_home_page(self):
+        response    =   self.client.get(reverse('index'))
+        self.assertEqual(response.status_code,200)
+class pizzaPageTestCase(TestCase):
+    def test_newPizza_added(self):
+        countPizza    =   PizzaModel.objects.count()
+        PizzaModel.objects.create(PizzaName='Italian Bbq Pizza',PizzapriceM=280,PizzapriceL=500,PizzaImage='https://img.freepik.com/premium-photo/bbq-pizza-topped-with-slices-spicy-italian-sausage-roasted-red-peppers-caramelized-onions_198067-250074.jpg')
+
+        self.assertEqual(PizzaModel.objects.count(),countPizza+1)
